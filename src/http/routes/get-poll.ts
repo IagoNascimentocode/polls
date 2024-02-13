f5859026877f4ea3572a7a3aa5e0373f02faf6cd
IagoNascimentocode <iago_ns@hotmail.com>
2024-02-12 21:50:23 -0300

    3 - Aula

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../../lib/prisma";
 import z from "zod";
+import { redis } from "../../lib/redis";
 
 export async function getPool(app: FastifyInstance) {
 
@@ -24,7 +25,35 @@ export async function getPool(app: FastifyInstance) {
       }
     });
 
-    return reply.send({ poll });
+    if (!poll) {
+      return reply.status(400).send({ mssage: "Poll not found" })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1]
+
+        Object.assign(obj, { [line]: Number(score) })
+      }
+
+      return obj
+    }, {} as Record<string, number>)
+
+    return reply.send({
+      poll: {
+        id: pollId,
+        titile: poll.title,
+        options: poll.options.map(option => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: (option.id in votes) ? votes[option.id] : 0
+          }
+        })
+      }
+    });
   });
 
 }
